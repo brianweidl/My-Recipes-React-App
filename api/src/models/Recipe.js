2395f538b260e72d72aa55460eb97b8e8f2df72d
brianweidl <brianweidl@outlook.com>
2022-01-31 17:33:22 -0300

    start repository

---
+++ api/src/models/Recipe.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize')
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+	// defino el modelo
+	sequelize.define('recipe', {
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			primaryKey: true,
+			allowNull: false,
+		},
+		summary: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		healthScore: {
+			type: DataTypes.FLOAT,
+		},
+		score: {
+			type: DataTypes.FLOAT,
+		},
+	})
+}
